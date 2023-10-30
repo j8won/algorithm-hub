b1d568e7601573088cfb03dda492bfbc34fcfb19
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2023-10-31 02:23:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.03 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,14 @@
+function solution(participant, completion) {
+    var answer = '';
+    participant.sort();
+    completion.sort();
+    
+    console.log(participant);
+    console.log(completion);
+    
+    for (let i = 0; i < participant.length; i++) {
+        if (participant[i] !== completion[i]) {
+            return participant[i];
+        }
+    }
+}
