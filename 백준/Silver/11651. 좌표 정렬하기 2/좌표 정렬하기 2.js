9d53f47e52971840f455c51fe737ec680ae7c778
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2024-05-25 16:56:58 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 448 ms, Memory: 54008 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,16 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = +input.shift();
+let array = input.map((x) => x.split(" ").map(Number));
+
+array.sort((a, b) => {
+  if (a[1] === b[1]) return a[0] - b[0];
+  else return a[1] - b[1];
+});
+
+array = array.map((x) => `${x[0]} ${x[1]}`);
+console.log(array.join("\n"));
