ce5fd28bd7efac735e229f95a2761c9efcbf50f9
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2025-08-07 23:22:09 +0900

    [level 2] Title: 기능개발, Time: 3.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,23 +1,29 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let days = [];
 
-    for (let i=0; i<progresses.length; i++) {
-        days.push(Math.ceil((100 - progresses[i]) / speeds[i]));
-    }
+    let days = [];
     
-    let max = days[0]
-    let maxDays = 1;
-    for (let i=1;i<days.length; i++){
-        if (max < days[i]) {
-            answer.push(maxDays);
-            max = days[i];
-            maxDays = 1;
-        } else {
-            maxDays++;
+    days = progresses.map((x, i) => Math.ceil((100 - x) / speeds[i]))
+    
+    console.log(days)
+    
+    
+    let left = 0;
+    let right = 1;
+    
+    while (right < days.length) {
+        if (days[left] >= days[right]) {
+            right++;
+            continue;
         }
         
-        if (i === days.length - 1) answer.push(maxDays)
+        answer.push(right - left)
+        left = right;
+        right = right + 1;
+    }
+    
+    if (left < right) {
+        answer.push(right-left)
     }
     
     return answer;
